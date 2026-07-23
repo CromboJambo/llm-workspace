187d5c7a5b41b5211a99646af13bfeecbccdf804
crombojambo <dustinjgrover@gmail.com>
2026-07-22 23:54:46 -0500

    clean build artifacts

--- llm-runner/examples/tcgen05_gemm/target/debug/deps/rustc_hash-6839b22da3369682.d
+++
@@ -1,8 +0,0 @@
-/home/crombo/crabjar/src/llm-runner/examples/tcgen05_gemm/target/debug/deps/rustc_hash-6839b22da3369682.d: /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/lib.rs /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/seeded_state.rs
-
-/home/crombo/crabjar/src/llm-runner/examples/tcgen05_gemm/target/debug/deps/librustc_hash-6839b22da3369682.rlib: /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/lib.rs /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/seeded_state.rs
-
-/home/crombo/crabjar/src/llm-runner/examples/tcgen05_gemm/target/debug/deps/librustc_hash-6839b22da3369682.rmeta: /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/lib.rs /home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/seeded_state.rs
-
-/home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/lib.rs:
-/home/crombo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-hash-2.1.2/src/seeded_state.rs:
--- llm-runner/examples/tcgen05_gemm/target/debug/deps/tcgen05_gemm-04ab1cc185cd79df.d
+++
@@ -1,5 +0,0 @@
-/home/crombo/crabjar/src/llm-runner/examples/tcgen05_gemm/target/debug/deps/tcgen05_gemm-04ab1cc185cd79df.d: src/main.rs
-
-/home/crombo/crabjar/src/llm-runner/examples/tcgen05_gemm/target/debug/deps/libtcgen05_gemm-04ab1cc185cd79df.rmeta: src/main.rs
-
-src/main.rs:
